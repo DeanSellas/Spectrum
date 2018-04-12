3a54503f921af8ae304d88675037dda491f845dd
Dean S <dgsellas@gmail.com>
2018-04-12 18:46:38 -0500

    init

--- _OLD/0.1.3/Spectrum/Program.cs
+++ _OLD/0.1.3/Spectrum/Program.cs
@@ -4,19 +4,16 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Spectrum
-{
-    static class Program
-    {
+namespace Spectrum {
+    static class Program {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
-        {
+        static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            try { Application.Run(new spectrumFormMain()); } catch { }
         }
     }
 }
